e7276b45b151a0976c26dd9a4084fefdbc945b67
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-02-05 19:32:16 +0545

    Added pagination

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,11 +1,85 @@
 import "./homepage.css";
+import { useState } from "react";
+import { Pagination } from '@mantine/core';
+
 export default function Homepage(){
 
+    //Pagination
+    const [activePage,setPage] = useState(1);
+    const [recordPerPage, setRecordPerPage] = useState(5);
 
-    function element(title,company,imgPath,location){
+    const [jobs,setJobs] = useState([{
+        title:"Blockchain intern",
+        company:"Binance",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern3",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern4",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern5",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern6",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern7",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern8",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern9",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    },{
+        title:"Solidity intern10",
+        company:"Consensys",
+        imgPath:"./binance.png",
+        location:"Remote"
+    }])
+
+    const lastIndex = activePage * recordPerPage;
+    const firstIndex = lastIndex - recordPerPage;
+    const currentRecords = jobs.slice(firstIndex,lastIndex);
+
+    //Get total number of jobs
+    function getTotalJobs(){
+
+        let count =0;
+        jobs.map(obj=>{
+            count++;
+            
+        })
+        return count;
+    }
+
+
+    function element(title,company,imgPath,location,index){
         return(
      
-            <div className="job-box">
+            <div className="job-box" key={index}>
                 <div className="job-first">
                     <div className="job-logo">
                         <img src={imgPath} alt="logo"/>
@@ -31,15 +105,26 @@ export default function Homepage(){
 
     }
 
+    const content = currentRecords.map((obj,i)=>{
+        return (
+            <>
+                {element(obj.title,obj.company,obj.imgPath,obj.location,i)}
+                <br/>
+            </>
+        )
+    })
+    console.log(activePage);
+
 
     return(
         <div className="homepage">
             <div className="job-container ">
-                {element("Blockchain intern","Binance","./binance.png","Remote")}
-                <br/>
-                {element("Solidity intern","Consensys","./binance.png","Remote")}
 
+                {content}
+
+                <Pagination page={activePage} onChange={setPage} total={Math.ceil(getTotalJobs()/5)}/>
             </div>
+
 
         </div>
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-
+import { MantineProvider } from '@mantine/core';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <MantineProvider  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MantineProvider>
 
 );
 
