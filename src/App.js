d85fc8118d3236cca9fbc68395130606d08eea5a
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-03-03 07:20:00 +0545

    Integrated Google Analytics

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import {Routes,Route, } from "react-router-dom";
 import Navbar from './components/navbar/navbar';
 import Homepage from './components/homepage/homepage';
 import Form from './components/Form/Form';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-0CPPKXHV58"; // TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="App">
       <Navbar/>
